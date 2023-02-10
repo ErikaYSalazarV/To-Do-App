47d30993fb79c11833bf451e4e091458f4c89f03
ErikaYSalazarV <123501982+ErikaYSalazarV@users.noreply.github.com>
2023-02-10 12:19:05 -0500

    Update sqlite.spec.js

--- app/spec/persistence/sqlite.spec.js
+++ app/spec/persistence/sqlite.spec.js
@@ -1,6 +1,6 @@
 const db = require('../../src/persistence/sqlite');
 const fs = require('fs');
-const location = process.env.SQLITE_DB_LOCATION || '/home/runner/work/To-Do-App/To-Do-App/app/todos/todos.db';
+const location = process.env.SQLITE_DB_LOCATION || '/home/runner/work/To-Do-App/To-Do-App/app/todos.db';
 
 const ITEM = {
     id: '7aef3d7c-d301-4846-8358-2a91ec9d6be3',
